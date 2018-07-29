b5e666beb5663ae8f94ef277f4ae876c2d57b31a
Rodrigo Souza Camelo <rodrigo.souza.camelo@gmail.com>
2018-07-28 22:30:32 -0300

    created the game looping

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let canvas, width, height;
+let canvas, width, height, fps;
 
 function init() {
     canvas = document.createElement("canvas");
@@ -6,13 +6,22 @@ function init() {
     canvas.height = 600;
 
     document.body.appendChild(canvas);
+    ctx = canvas.getContext("2d");
+    
+    fps = 15;
+
+    run();
 }
 
-function update() {}
+function update() {
+
+}
 
 function run() {
     update();
     draw();
+
+    setTimeout(run, 1000/fps);
 }
 
 function draw() {}
